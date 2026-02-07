3e82aa71be4bff7c1ceae1cc452fa5839ea0a962
zawtunwai <250202286+zawtunwai@users.noreply.github.com>
2026-02-07 09:20:53 +0700

    Update config_lock.sh

--- config_lock.sh
+++ config_lock.sh
@@ -3,5 +3,5 @@
 # GitHub: https://github.com/zawtunwai/maungthunya-vip
 # EDIT THIS FILE TO ALLOW SPECIFIC IPs
 
-ALLOWED_IPS=("43.252.21.107" "157.85.96.37" "23.189.104.199" "82.25.47.186")  # ဒီမှာ ဝယ်သူရဲ့ IP ထည့်ပါ
+ALLOWED_IPS=("43.252.21.107" "157.85.96.37" "23.189.104.199" "82.25.47.186")  
 LICENSE_KEY="MAUNGTHUNYA_2026_VIP"  # license key
